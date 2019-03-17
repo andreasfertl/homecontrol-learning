40e171020965590ea31a400da82a3abf852a86d6
Andreas Fertl <andreas.fertl@gmail.com>
2019-03-17 12:32:57 +0100

    removed demonstration of unorderd printouts again

--- homecontrol-learning/logger.cpp
+++ homecontrol-learning/logger.cpp
@@ -2,17 +2,6 @@
 #include <string>
 #include "logger.h"
 #include "iRun.h"
-#include <sstream>
-#include <thread>
-
-namespace {
-	std::wstring getThreadId() {
-		std::wstringstream wss;
-		std::this_thread::get_id()._To_text(wss);
-
-		return wss.str();
-	}
-}
 
 logger::logger(struct iRun& IRun) :
 	m_SerializeFunctionCalls(IRun)
@@ -22,6 +11,6 @@ logger::logger(struct iRun& IRun) :
 void logger::logg(const std::wstring& str)
 {
 	m_SerializeFunctionCalls.run([str]() {
-		std::wcout << L"called from threadId: " << getThreadId() << L" " << str;
+		std::wcout << str;
 	});
 }
--- homecontrol-learning/programManager.cpp
+++ homecontrol-learning/programManager.cpp
@@ -5,25 +5,6 @@
 #include "logger.h"
 #include "logging.h"
 #include "seriealizeFunctions.h"
-#include "iRun.h"
-
-namespace {
-
-	class unorderdFunctionCalls : public iRun {
-	public:
-		//implementation of iRun
-		void run(const std::function<void()>&& func) override { func(); };
-		void run(const std::function<void()>& func) override { func(); };
-	};
-
-	std::wstring getThreadId() {
-		std::wstringstream wss;
-		std::this_thread::get_id()._To_text(wss);
-
-		return wss.str();
-	}
-}
-
 
 class programManagerImpl {
 
@@ -31,25 +12,9 @@ public:
 	programManagerImpl() :
 		m_Run(true),
 		m_SerializeFunctionCalls(),
-		m_UnorderdFunctionCalls(),
-		m_LoggManager(m_UnorderdFunctionCalls)
+		m_LoggManager(m_SerializeFunctionCalls)
 	{
 		_logg(m_LoggManager, L"Startup");
-
-
-		std::vector<std::thread> threads;
-		//spawn threads
-		for (int i = 0; i < 10; i++)
-		{
-			threads.emplace_back([this]() {
-				_logg(this->m_LoggManager, L"Printout from thread id: " + getThreadId());
-			});
-		}
-
-
-		// join/wait for the end of the threads
-		for (auto& t : threads)
-			t.join();
 	}
 
 	void run()
@@ -76,7 +41,6 @@ public:
 private:
 	std::atomic<bool>   m_Run;
 	seriealizeFunctions m_SerializeFunctionCalls;
-	unorderdFunctionCalls m_UnorderdFunctionCalls;
 	logger              m_LoggManager;
 
 };
